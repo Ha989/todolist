c11dc263de1e6cbe957a94c652552dbeb22d2856
Ha989 <viethanguyen0809@gmail.com>
2022-12-03 17:10:16 +0700

    add refactor React for todo List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import Header from "./components/Header";
 import './App.css';
+import TaskList from './components/TaskList';
+import AddTaskForm from './components/AddTaskForm';
 
 function App() {
     const [tasks, setTasks] = useState([
         {id:"task_1", title: 'Learn JS and React', status: 0},
         {id:"task_2", title: 'Code a Todo List', status: 0},
     ]);
-    const [showIncomplete, setShowIncomplete] = useState(true);
+    const [showIncomplete, setShowIncomplete] = useState(false);
     const [newTask, setNewTask] = useState("");
 
     const handleSubmit = (e) => {
@@ -42,33 +45,10 @@ function App() {
 
   return (
     <div className="container">
-  <h1 className="title">Todo List
-   <span>Get one item done at a time.</span></h1>
- 
-  <ul className="task-list">
-    {tasks.filter(task => showIncomplete ? task.status !== 1 : true)
-    .map(task => (
-    <li key={task.id} className={task.status ? "done" : ""}>
-      <span className="label">{task.title}</span>
-      <div className="actions">
-        <input type="checkbox" className="btn-action btn-action-done" 
-        checked={Boolean(task.status)}
-        onChange={(e) => setTaskStatus(task.id, e.target.checked)}/>
-        <button onClick={() => removeTask(task.id)} className="btn-action btn-action-delete">✖</button>
-      </div>
-    </li>
-    ))}
+    <Header title="Todo List" subTitle="Get one item done at a time."/>
+    <TaskList tasks={tasks} showIncomplete={showIncomplete} setTaskStatus={setTaskStatus} removeTask={removeTask} setShowIncomplete={setShowIncomplete}/>
     
-  </ul>
-  <div className="filter-wrapper">
-    <label htmlFor="filter" className="filter-label">Show incompleted tasks only</label>
-    <input type="checkbox" id="filter" checked={showIncomplete} onChange={(e) => setShowIncomplete(e.target.checked)}></input>
-  </div>
-  <form onSubmit={handleSubmit} className="form">
-    <label htmlFor="newitem">Add to the Todo list</label>
-    <input type="text" id="newitem" value={newTask} onChange={handleInputChange}></input>
-    <button type="submit">Add Item</button>
-  </form>
+    <AddTaskForm newTask={newTask} handleSubmit={handleSubmit} handleInputChange={handleInputChange}/>
 </div>
   );
 }
